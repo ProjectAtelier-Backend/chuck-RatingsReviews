0003eb90a963fd8c7a6c9ffad3b469c035f38981
Chuck Song <chuckthedeveloper@gmail.com>
2022-11-28 22:37:49 -0500

    Defined the routes expected by API

--- server/index.js
+++ server/index.js
@@ -9,3 +9,33 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
+
+
+
+// ---- RATINGS & REVIEWS ROUTES ---- //
+
+//Returns a list of reviews for a particular product
+app.get('/reviews', (req, res) => {
+  res.end();
+});
+
+//Returns review metadata for a given product
+app.get('/reviews/meta', (req, res) => {
+  res.end();
+});
+
+//Adds a review for the given product
+app.post('/reviews', (req, res) => {
+  res.end();
+});
+
+//Updates a review to show it was found helpful
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  res.end();
+});
+
+//Updates a review to show it was reported
+app.put('/reviews/:review_id/report', (req, res) => {
+  res.end();
+});
+
